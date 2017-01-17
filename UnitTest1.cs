ba1d614450ecc0bcebb4fb74c8017b616f0f0676
yumetokiseki <yumet@LAPTOP-V8EAP409>
2017-01-17 23:01:06 +0800

    Scenario6

--- UnitTest1.cs
+++ UnitTest1.cs
@@ -107,6 +107,27 @@ namespace TDD_D2
             //assert
             Assert.AreEqual(expected, actual);
         }
+
+        [TestMethod]
+        public void Scenario6()
+        {
+            //arrange
+            var target = new ShoppingCart();
+            var expected = 370;
+            var books = new List<Book>
+            {
+                new Book { Name= "第一集" , Price = 100 },
+                new Book { Name= "第二集" , Price = 100 },
+                new Book { Name= "第三集" , Price = 100 },
+                new Book { Name= "第三集" , Price = 100 }
+            };
+
+            //act
+            var actual = target.calculate(books, 5, 10, 20, 25);
+
+            //assert
+            Assert.AreEqual(expected, actual);
+        }
     }
 
     public class Book
