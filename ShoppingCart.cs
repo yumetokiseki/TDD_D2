778925ced2ad0278cc6127cc94b17cf8d55b5dfb
yumet <yumet@LAPTOP-V8EAP409>
2017-01-17 19:42:58 +0800

    Add project files.

---
+++ ShoppingCart.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TDD_D2
+{
+    public class ShoppingCart
+    {
+        public ShoppingCart()
+        {
+        }
+
+        internal object calculate(List<Book> books, int v1, int v2, int v3, int v4)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ UnitTest1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+
+namespace TDD_D2
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            //arrange
+            var target = new ShoppingCart();
+            var expected = 100;
+            var books = new List<Book>
+            {
+                new Book { Name= "第一集" , Price = 100 }
+            };
+
+            //act
+            var actual = target.calculate(books, 5,10,20,25);
+
+            //assert
+            Assert.AreEqual(expected, actual);
+
+        }
+    }
+
+    public class Book
+    {
+        public string Name { get; set; }
+        public int Price { get; set; }
+    }
+}
