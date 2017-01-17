9198a989f2044c38c5c606f20d75c7d555dfa57c
yumetokiseki <yumet@LAPTOP-V8EAP409>
2017-01-17 22:15:13 +0800

    Scenario2 Complete

--- ShoppingCart.cs
+++ ShoppingCart.cs
@@ -9,14 +9,20 @@ namespace TDD_D2
         {
         }
 
-        internal int calculate(List<Book> books, int v1, int v2, int v3, int v4)
+        internal double calculate(List<Book> books, int discountby2, int discountby3, int discountby4, int discountby5)
         {
-            int result = 0;
+            double result = 0;
 
             foreach (Book book in books)
             {
                 result += book.Price;
             }
+
+            if (books.Count == 2)
+            {
+                result = result * (100 - discountby2) / 100;
+            }
+
             return result;
         }
     }
